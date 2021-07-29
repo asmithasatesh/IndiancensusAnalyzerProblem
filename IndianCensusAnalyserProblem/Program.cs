4bef047ad9a85a48ae9ebf966d216125726ef8b6
asmithasatesh <asmithasatesh@gmail.com>
2021-07-29 11:07:19 +0530

    [Asmitha]Initial commit with Welcome Message

---
+++ IndianCensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianCensusAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian Census Analyzer!");
+        }
+    }
+}
